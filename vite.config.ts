8cb8a20836c95e801adcc6b32e586048b65afb3a
AbelAlvarz <abel.alvarez.dev@outlook.com>
2025-02-01 15:22:55 -0600

    removed unused path import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
