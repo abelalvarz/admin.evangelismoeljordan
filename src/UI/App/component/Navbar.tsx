dbdebc13231584a1a731e9978369a28955a35525
AbelAlvarz <abel.alvarez.dev@outlook.com>
2025-06-11 16:48:19 -0600

    remove wrong import

--- src/UI/App/component/Navbar.tsx
+++ src/UI/App/component/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { PrivateRoutes } from '../config/AppRoutes'
